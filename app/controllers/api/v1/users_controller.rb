4716868b1b6e2ac198c0cb87a5c528d06b9a8185
Guilherme Sehn <ghsehn@gmail.com>
2015-09-29 23:21:23 -0300

    Session is now created with the user

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -15,7 +15,7 @@ module Api
         @user = User.new(user_params)
 
         if @user.save
-          render json: @user
+          render json: { user: @user, session: @user.create_session }
         else
           render json: @user.errors, status: :unprocessable_entity
         end
