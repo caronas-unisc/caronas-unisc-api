55fe60f8a7b38fdd20b0cdfab55238ad809ade4d
Guilherme Sehn <ghsehn@gmail.com>
2015-11-23 00:57:24 -0200

    remove unused method

--- app/models/ride.rb
+++ app/models/ride.rb
@@ -6,13 +6,6 @@ class Ride < ActiveRecord::Base
 
   enum status: [:pending, :accepted]
 
-  def self.create_ride_for_availability(user, giver_availability)
-    user.ride_availabilities.receive.where(
-      period: RideAvailabilities.periods[:night],
-      
-    )
-  end
-
   def as_json(options = {})
     super(only: [:id, :status])
   end
