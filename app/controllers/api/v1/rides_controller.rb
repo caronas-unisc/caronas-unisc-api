e721eb7bacc2d5d1a6b710e962df5cfd680b3e88
Guilherme Sehn <ghsehn@gmail.com>
2015-11-27 15:01:38 -0200

    ride bug fix

--- app/controllers/api/v1/rides_controller.rb
+++ app/controllers/api/v1/rides_controller.rb
@@ -15,8 +15,8 @@ module Api
         ride = Ride.find(params[:id])
         render json: {
           ride: ride,
-          giver_availability: ride.giver_availability.includes(:user),
-          receiver_availability: ride.receiver_availability.includes(:user)
+          giver_availability: ride.giver_availability,
+          receiver_availability: ride.receiver_availability
         }
       end
 
