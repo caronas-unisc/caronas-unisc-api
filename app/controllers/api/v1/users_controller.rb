a3849911a162878dde1a25a4d48055069de969b5
Guilherme Sehn <ghsehn@gmail.com>
2015-11-29 19:28:46 -0200

    remove show user endpoint

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -8,11 +8,6 @@ module Api
         render json: users
       end
 
-      def show
-        user = User.find(params[:id])
-        render json: user
-      end
-
       def create
         user = User.new(create_user_params)
 
